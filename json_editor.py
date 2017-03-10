3421b211f1af85a3b08fb4edabd8205534530718
Inkognita <liepieshov@ucu.edu.ua>
2017-03-11 01:18:25 +0200

    js_editor loading

--- json_editor.py
+++ json_editor.py
@@ -13,3 +13,36 @@ with open(input_file, 'r') as data_file:
 
 with open(output_file, 'w') as outfile:
     json.dump(data, outfile)
+
+def type_recogniser(file_):
+    if file_['humidity']['percent']:
+        pass
+    elif file_['humidity']['percent']:
+        pass
+    else:
+        pass
+    # if file_['date']['local']:
+    #     pass
+    # elif file_['date']['local']:
+    #     pass
+    # else:
+    #     pass
+    file_['cloudiness'] = file_['cloudiness']['type']
+    if file_['precipitation']['amount']:
+        pass
+    elif file_['precipitation']['amount']:
+        pass
+    else:
+        pass
+    # if file_['temperature']['air']:
+    #     pass
+    # elif file_['temperature']['air']:
+    #     pass
+    # else:
+    #     pass
+    if 5 <= file_['wind']['speed'] <= 10:
+        file_['wind'] = 3
+    elif file_['wind']['speed'] > 10:
+        file_['wind'] = 4
+    else:
+        file_['wind'] = 2
