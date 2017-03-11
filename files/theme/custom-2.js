4abdd496809b114113c6d7d90270f3a98ab78605
Inkognita <liepieshov@ucu.edu.ua>
2017-03-11 16:03:27 +0200

    JS

--- files/theme/custom-2.js
+++ files/theme/custom-2.js
@@ -1,5 +1,34 @@
+function calculate_date(zm) {
+        var today = new Date();
+var dd = today.getDate() + zm;
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd
+}
+
+if(mm<10) {
+    mm='0'+mm
+}
+
+//console.log(yyyy+ '-' + mm + '-' + dd);
+return yyyy+ '-' + mm + '-' + dd;
+    }
+ $.getJSON('info.json', function (data) {
+        var a = 505;
+        for (var i=0; i<=6; i++){
+            //console.log(changing);
+            a = '2017-03-0' + (i + 1);
+            //console.log(a);
+            //console.log(data.changing);
+            //console.log('div#Photo' + (i + 1) + ' > a > img');
+            //console.log(a, 'LOL=)');
+            $('div#Photo' + (i + 1) + ' > a > img').attr('src', './levels/' + data[a][1] + '.png');
+        }
+    });
 function main() {
-    var data = 'A';
+    var changingNew;
     $('#custom_window_sec').hide();
     $('#custom_window').css('text-size', 50);
     //$('#custom_window').hide();
@@ -20,7 +49,5 @@ function main() {
         });
         $('#Photo' + i).css("cursor", "pointer");
     };
-   $.getJSON('info.json', function(data) {
-    console.log(data.new, data.A.B);
-});};
+   };
 $(document).ready(main);
