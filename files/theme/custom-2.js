0932a15b7f056b372efdd4f58c530e92a9256136
Inkognita <liepieshov@ucu.edu.ua>
2017-03-11 06:14:15 +0200

    'Commmmit'

--- files/theme/custom-2.js
+++ files/theme/custom-2.js
@@ -1,8 +1,13 @@
 function main() {
+    var data = 'A';
+    $('#custom_window').css('text-size', 50);
     //$('#custom_window').hide();
-    for (var i = 1; i <= 6; i++){
+    for (var i = 1; i <= 7; i++){
     $('#Photo' + i).bind('click', function () {
-        $('#custom_window').slideToggle(400);
-    })}
+        $('#custom_window').hide(700);
+        $('#custom_window').slideToggle(500);
+    });
+    $('#Photo' + i).css("cursor", "pointer");
+    }
 }
 $(document).ready(main);
