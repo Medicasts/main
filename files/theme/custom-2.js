4f4504c60e0c90966399ef58b1090f679408cf61
Inkognita <liepieshov@ucu.edu.ua>
2017-03-16 15:45:10 +0200

    Loading...

--- files/theme/custom-2.js
+++ files/theme/custom-2.js
@@ -1,70 +1,76 @@
 function main() {
     var changingNew;
     //start
-    google.charts.load('current', {'packages':['bar']});
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Date', 'humidity', 'pressure', 'precipation'],
-        ['8', 10, 20, 12],
-        ['9', 11, 4, 25],
-        ['10', 20, 11, 3],
-        ['11', 17, 5, 3],
-        ['12', 10, 20, 12],
-        ['13', 11, 4, 25],
-        ['14', 20, 11, 3],
-        ['15', 17, 5, 3],
-        ['16', 10, 20, 12],
-        ['17', 11, 4, 25],
-        ['18', 20, 11, 3],
-        ['19', 17, 5, 3],
-        ['20', 10, 20, 12],
-        ['21', 11, 4, 25],
-        ['22', 20, 11, 3],
-        ['23', 17, 5, 3],
-        ['24', 10, 20, 12],
-        ['25', 11, 4, 25],
-        ['26', 20, 11, 3],
-        ['27', 17, 5, 3],
-        ['28', 10, 20, 12],
-        ['1', 11, 4, 25],
-        ['2', 20, 11, 3],
-        ['3', 17, 5, 3],
-        ['4', 11, 4, 25],
-        ['5', 20, 11, 3],
-        ['6', 17, 5, 3],
-        ['7', 10, 20, 12],
-        ['8', 11, 4, 25],
-        ['9', 20, 11, 3],
-        ['10', 17, 5, 3]
-    ]);
+    google.charts.load("43", {packages: ["corechart", "gauge", "table", "timeline", "bar"]});
+    //google.charts.load('current', {'packages':['bar']});
+    google.charts.setOnLoadCallback(drawChart);
 
-    var options = {
-        chart: {
-            title: 'Monthly history',
-            subtitle: 'factors: humidity, pressure, precipation',
-        },
-        bars: 'vertical',
-        vAxis: {format: 'decimal'},
-        height: 400,
-        width: 1000,
-        colors: ['#1b9e77', '#d95f02', '#7570b3']
-    };
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Date', 'humidity', 'pressure', 'precipation'],
+            ['8', 10, 20, 12],
+            ['9', 11, 4, 25],
+            ['10', 20, 11, 3],
+            ['11', 17, 5, 3],
+            ['12', 10, 20, 12],
+            ['13', 11, 4, 25],
+            ['14', 20, 11, 3],
+            ['15', 17, 5, 3],
+            ['16', 10, 20, 12],
+            ['17', 11, 4, 25],
+            ['18', 20, 11, 3],
+            ['19', 17, 5, 3],
+            ['20', 10, 20, 12],
+            ['21', 11, 4, 25],
+            ['22', 20, 11, 3],
+            ['23', 17, 5, 3],
+            ['24', 10, 20, 12],
+            ['25', 11, 4, 25],
+            ['26', 20, 11, 3],
+            ['27', 17, 5, 3],
+            ['28', 10, 20, 12],
+            ['1', 11, 4, 25],
+            ['2', 20, 11, 3],
+            ['3', 17, 5, 3],
+            ['4', 11, 4, 25],
+            ['5', 20, 11, 3],
+            ['6', 17, 5, 3],
+            ['7', 10, 20, 12],
+            ['8', 11, 4, 25],
+            ['9', 20, 11, 3],
+            ['10', 17, 5, 3]
+        ]);
 
-    var chart = new google.charts.Bar(document.getElementById('custom_window_three'));
+        var options = {
+            chart: {
+                title: 'Monthly history',
+                subtitle: 'factors: humidity, pressure, precipation'
+            },
+            bars: 'vertical',
+            vAxis: {format: 'decimal'},
+            height: 400,
+            width: 1000,
+            colors: ['#1b9e77', '#d95f02', '#7570b3'],
+            allowHtml: true
+        };
 
-    chart.draw(data, google.charts.Bar.convertOptions(options));
+        var chart = new google.charts.Bar(document.getElementById('custom_window_three'));
 
-    /*var btns = document.getElementById('btn-group');
+        chart.draw(data, google.charts.Bar.convertOptions(options));
 
-    btns.onclick = function (e) {
+        /*var btns = document.getElementById('btn-group');
 
-        if (e.target.tagName === 'BUTTON') {
-            options.vAxis.format = e.target.id === 'none' ? '' : e.target.id;
-            chart.draw(data, google.charts.Bar.convertOptions(options));
-        }
-    }*/
-}
+         btns.onclick = function (e) {
+
+         if (e.target.tagName === 'BUTTON') {
+         options.vAxis.format = e.target.id === 'none' ? '' : e.target.id;
+         chart.draw(data, google.charts.Bar.convertOptions(options));
+         }
+         }*/
+    }
+
+    $('div#custom_window_three > div').css('margin-left', 'auto');
+    $('div#custom_window_three > div').css('margin-right', 'auto');
 
     //end
     function calculate_date(zm) {
@@ -105,7 +111,7 @@ function drawChart() {
     });
     var elmntSec = document.getElementById("custom_window_sec");
     var elmnt = document.getElementById("custom_window");
-    $('#custom_window_sec').hide();
+    //$('#custom_window_sec').hide();
     $('#custom_window').css('text-size', 50);
     //$('#custom_window').hide();
     $('#show_graph').bind('click', function () {
@@ -117,19 +123,20 @@ function drawChart() {
     for (var i = 1; i <= 7; i++) {
         $('#Photo' + i).bind('click', function () {
             $.getJSON('info.json', function (data) {
-        $('#text3').text("Група максимального впливу: " + data['2017-03-0' + i][3][0]);
-        $('#text4').text("Основні фактори: " + data['2017-03-0' + i][3][1]);
-        });
+                $('#text3').text("Група максимального впливу: " + data['2017-03-0' + i][3][0]);
+                $('#text4').text("Основні фактори: " + data['2017-03-0' + i][3][1]);
+            });
             $('#custom_window').hide();
             //$('#custom_window_sec').hide();
 
 
-            //$('#custom_window').scrollIntoView()
+            //$('#custom_window').scrollIntoView();
             $('#custom_window').slideToggle(500);
             //elmnt.scrollIntoView({block: "end", behavior: "smooth"});
         });
         $('#Photo' + i).css("cursor", "pointer");
-    }
-    ;
+    };
+$('div#custom_window_three > div').css('margin-left', 'auto');
+    $('div#custom_window_three > div').css('margin-right', 'auto');
 };
 $(document).ready(main);
