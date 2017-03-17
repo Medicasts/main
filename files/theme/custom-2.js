749c07892c4e60d1ee145db7dfa3162522793de7
Inkognita <liepieshov@ucu.edu.ua>
2017-03-17 18:42:38 +0200

    Please, don't change anything=)

--- files/theme/custom-2.js
+++ files/theme/custom-2.js
@@ -59,8 +59,32 @@ function main() {
                 // console.log(a);
                 // console.log(data[a][3][0]);
                 // console.log(data[a][3][1]);
-                $('#text3').text("Основні фактори: " + data_spec[0]);
-                $('#text4').text("Група максимального впливу: " + data_spec[1]);
+            var table_text = '<tr><td rowspan="'+ data_spec[0].length+  '" style="font-weight: bold;">Основні фактори: </td>';
+            // console.log(data['2017-04-' + startPoint][3][1][0].join('<br>') + '</td></tr>');
+            for (var i = 0; i < data_spec[0].length; i++){
+                if (i == 0) {
+                    table_text += '<td>' + data_spec[0][i] + '</td></tr>'
+                }
+                else {
+                    table_text += '<tr><td>' + data_spec[0][i] + '</td></tr>'
+                }
+            }
+            table_text +='<tr><td rowspan="'+ data_spec[1].length + '" style="font-weight: bold; border-bottom: 0;">Групи ризику: </td>';
+
+            for (var i = 0; i < data_spec[1].length; i++){
+                if (i == 0) {
+                    table_text += '<td>' + data_spec[1][i].join('<br>') + '</td></tr>'
+                }
+                else {
+                    table_text += '<tr><td>' + data_spec[1][i].join('<br>') + '</td></tr>'
+                }
+            }
+            // console.log(12345);
+            // console.log(table_text);
+            $('#custom_window tbody').html(table_text);
+
+                // $('#text3').text("Основні фактори: " + data_spec[0]);
+                // $('#text4').text("Група максимального впливу: " + data_spec[1]);
                 $(window).slideToggle(500);
         }
     }
@@ -70,58 +94,59 @@ function main() {
         var d = new Date(parseInt(splitted_date[0]), parseInt(splitted_date[1]) - 1, parseInt(splitted_date[2]));
         return weekday[d.getDay()];
     }
-    var startPoint = 23;
+    var startPoint = 1;
+    var startPointStr;
+    if (startPoint < 10) {
+        startPointStr = '0' + startPoint;
+    }
+    else {
+        startPointStr = startPoint.toString();
+    }
     var window = document.getElementById('custom_window');
     $.getJSON('info.json', {}, function (data) {
+        // console.log(data);
     // $.getJSON('https://medicasts.herokuapp.com/data.json', {}, function (data) {
-
-        if (startPoint <= 9){$('#text1').text("Основні фактори: " + data['2017-03-0' + startPoint][3][0]);
-        $('#text2').text("Група максимального впливу: " + data['2017-03-0' + startPoint][3][1]);
-        }
-        else{
-        $('#text1').text("Основні фактори: " + data['2017-03-' + startPoint][3][0]);
-        $('#text2').text("Група максимального впливу: " + data['2017-03-' + startPoint][3][1]);
-            }
- /*       var table_text ='<tr><td rowspan="'+ data['2017-03-' + startPoint][3][0].length+  '">Онсновні фактори: </td>';
-
-        for (var i = 0, i < data['2017-03-' + startPoint][3][1].length, i++){
+       var table_text = '<tr><td rowspan="'+ data['2017-04-' + startPointStr][3][0].length+  '" style="font-weight: bold;">Основні фактори: </td>';
+        // console.log(data['2017-04-' + startPoint][3][1][0].join('<br>') + '</td></tr>');
+        for (var i = 0; i < data['2017-04-' + startPointStr][3][0].length; i++){
             if (i == 0) {
-                table_text += '<td>' + data['2017-03-' + startPoint][3][0][i] + '</td></tr>'
+                table_text += '<td>' + data['2017-04-' + startPointStr][3][0][i] + '</td></tr>'
             }
             else {
-                table_text += '<tr><td>' + data['2017-03-' + startPoint][0][i] + '</td></tr>'
+                table_text += '<tr><td>' + data['2017-04-' + startPointStr][3][0][i] + '</td></tr>'
             }
         }
-        table_text +='<tr><td rowspan="'+ data['2017-03-' + startPoint][3][1].length + '">Групи ризику: </td>';
+        table_text +='<tr><td rowspan="'+ data['2017-04-' + startPointStr][3][1].length + '" style="font-weight: bold; border-bottom: 0;">Групи ризику: </td>';
 
-        for (var i = 0, i < data['2017-03-' + startPoint][3][1].length, i++){
+        for (var i = 0; i < data['2017-04-' + startPointStr][3][1].length; i++){
             if (i == 0) {
-                table_text += '<td>' + data['2017-03-' + startPoint][3][1][i] + '</td></tr>'
+                table_text += '<td>' + data['2017-04-' + startPointStr][3][1][i].join('<br>') + '</td></tr>'
             }
             else {
-                table_text += '<tr><td>' + data['2017-03-' + startPoint][3][1][i] + '</td></tr>'
+                table_text += '<tr><td>' + data['2017-04-' + startPointStr][3][1][i].join('<br>') + '</td></tr>'
             }
         }
+        // console.log(12345);
         $('#day_block tbody').append(table_text);
-*/
 
-        $('#day_text').text(data['2017-03-' + startPoint][1].toString() + ' тип небезпеки');
+        $('#day_text').text(data['2017-04-' + startPointStr][1].toString() + ' тип небезпеки');
         for (var i = 0; i <= 6; i++) {
-            if (startPoint + i <= 9) {a = '2017-03-0' + (startPoint + i).toString()}
+            if (startPoint + i <= 9) {a = '2017-04-0' + (startPoint + i).toString()}
             else{
-            a = '2017-03-' + (startPoint + i).toString();
+            a = '2017-04-' + (startPoint + i).toString();
             // console.log(a);
             }
-            console.log(getDayName(a));
+            // console.log(getDayName(a));
             if (i > 0) {
                 $('#Table_Days tr td:nth-child(' + i.toString() + ')').text(getDayName(a));
             }
             $('div#Photo' + (i + 1) + ' > a > img').attr('src', './levels/' + data[a][1].toString() + '.png');
-            $('div#Photo' + (i + 1).toString()).bind('click', bindImages(data[a][3]));
+            $('div#Photo' + (i + 1).toString()).on('click', bindImages(data[a][3]));
         }
         });
+
     var elmntSec = document.getElementById("custom_window_sec");
-    $('#show_graph').bind('click', function () {
+    $('#show_graph').on('click', function () {
         $('#custom_window_sec').slideToggle(500);
         elmntSec.scrollIntoView();
         });
